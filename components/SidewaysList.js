a055b774a514ddbb06be914585162c7da48b976b
Sander <sanderlandm@gmail.com>
2025-07-04 13:31:32 +0200

    ADD basic version of sideways lists for location detail screen

---
+++ components/SidewaysList.js
@@ -0,0 +1,42 @@
+import {FlatList, Pressable, Text, View} from "react-native";
+import {useSettings} from "../contexts/SettingsContext";
+import getStyle from "./StyleSheet";
+import SidewaysListItem from "./SidewaysListItem";
+
+
+export default function SidewaysList({title, items, locationId, indexScreen, navigator}) {
+
+    const {settings} = useSettings();
+
+    const styles = getStyle(settings.theme);
+
+    const goToIndex = () => {
+
+        navigator.navigate(indexScreen, {locationId: locationId});
+
+    }
+
+    return (
+        <View style={styles.container}>
+
+            <Text style={styles.text}>{title}</Text>
+
+            <View style={styles.container}>
+
+                <FlatList
+                    horizontal={true}
+                    data={items}
+                    renderItem={({item}) => <SidewaysListItem key={item.id} item={item} type={title}/>}
+
+                />
+
+                <Pressable onPress={goToIndex}>
+                    <Text style={styles.text}>...</Text>
+                </Pressable>
+
+            </View>
+
+        </View>
+    )
+
+}
--- screens/locations/LocationScreen.js
+++ screens/locations/LocationScreen.js
@@ -1,11 +1,12 @@
 import getStyle from "../../components/StyleSheet"
-import {View, Text} from "react-native";
+import {View, Text, Pressable} from "react-native";
 import {useSettings} from "../../contexts/SettingsContext.js";
 import { useTranslation } from 'react-i18next';
 import {useEffect} from "react";
 import {useNavigation} from "@react-navigation/native";
 import i18next from "i18next";
 import TitledList from "../../components/TitledList";
+import SidewaysList from "../../components/SidewaysList";
 
 export default function LocationScreen({route}) {
 
@@ -25,6 +26,12 @@ export default function LocationScreen({route}) {
 
     const styles = getStyle(theme);
 
+    const goToMap = () => {
+
+        navigator.navigate('Map', {location: location});
+
+    }
+
     useEffect(() => {
         i18next.changeLanguage(settings.language);
         navigator.setOptions({title: t('LOCATION.TITLE')});
@@ -37,6 +44,26 @@ export default function LocationScreen({route}) {
             <TitledList items={location.facilities} title={t('LOCATION.FACILITIES')} translateItems={true}/>
 
             <TitledList items={location.institutions} title={t('LOCATION.INSTITUTIONS')}/>
+
+            {/*<SidewaysList */}
+            {/*    items={notes} */}
+            {/*    indexScreen={'Notes'} */}
+            {/*    locationId={location.id} */}
+            {/*    navigator={navigator} */}
+            {/*    title={'NOTES'}*/}
+            {/*/>*/}
+
+            {/*<SidewaysList */}
+            {/*    items={photos} */}
+            {/*    indexScreen={'Photos'} */}
+            {/*    locationId={location.id} */}
+            {/*    navigator={navigator} */}
+            {/*    title={'PHOTOS'}*/}
+            {/*/>*/}
+
+            <Pressable onPress={goToMap}>
+                <Text style={styles.text}>{t('LOCATION.MAP_BUTTON')}</Text>
+            </Pressable>
 
         </View>
     );
