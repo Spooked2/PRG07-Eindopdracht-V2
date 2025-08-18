aad8964bd6f7251a356d5544ac05c12634e3476a
sander <sanderlandm@gmail.com>
2025-08-18 21:35:07 +0200

    ADD extremely basic note creation screen

---
+++ screens/notes/NoteCreateScreen.js
@@ -0,0 +1,39 @@
+import getStyle from "../../components/StyleSheet"
+import {View, Text} from "react-native";
+import {useSettings} from "../../contexts/SettingsContext.js";
+import { useTranslation } from 'react-i18next';
+import {useNavigation} from "@react-navigation/native";
+import {useEffect, useState} from "react";
+import i18next from "i18next";
+
+export default function NoteCreateScreen() {
+
+    const {t} = useTranslation();
+
+    const navigator = useNavigation();
+
+    const {settings} = useSettings();
+
+    const {theme} = settings;
+
+    const styles = getStyle(theme);
+
+    const {formData, setFormData} = useState({
+        note: '',
+        locationId: 0,
+        id: Math.floor(Date.now() * Math.random()),
+        favorite: false
+    });
+
+    useEffect(() => {
+        i18next.changeLanguage(settings.language);
+        navigator.setOptions({title: t('NOTES.TITLE')});
+    }, [settings]);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{t('NOTES.TITLE')}</Text>
+        </View>
+    );
+
+}
