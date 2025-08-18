2761ad4f0b79f52b36550cb6bcdd2371c49e2ced
sander <sanderlandm@gmail.com>
2025-08-18 20:31:26 +0200

    ADD Installed expo locations and made minor edits to map screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,13 +1,13 @@
 import getStyle from "../components/StyleSheet"
-import {View, Text} from "react-native";
+import {View} from "react-native";
 import {useSettings} from "../contexts/SettingsContext.js";
 import {useTranslation} from 'react-i18next';
 import {useNavigation} from "@react-navigation/native";
 import {useEffect} from "react";
 import i18next from "i18next";
-import MapView from "react-native-maps";
-import {Marker} from "react-native-maps";
+import MapView, {Marker} from "react-native-maps";
 import {useLocations} from "../contexts/LocationsContext";
+import * as Location from 'expo-location';
 
 export default function MapScreen() {
 
@@ -36,14 +36,16 @@ export default function MapScreen() {
                 initialRegion={{
                     latitude: 51.91720484564559,
                     longitude: 4.4840949657534805,
-                    latitudeDelta: 0.01,
-                    longitudeDelta: 0.01
+                    latitudeDelta: 0.002,
+                    longitudeDelta: 0.002
                 }}
+                mapType={"standard"}
+                showsUserLocation={true}
             >
 
-                {locations.map((location, index) => (
+                {locations.map(location => (
                         <Marker
-                            key={index}
+                            key={location.id}
                             coordinate={location.coordinates}
                             title={location.name}
                             description={''}
