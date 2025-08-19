b9a5b6a667cbe324e05515699ec8c0443d5ba327
sander <sanderlandm@gmail.com>
2025-08-19 19:59:26 +0200

    FIX notes screen dropdown now shows the correct item when navigating from a location screen

--- screens/notes/NotesScreen.js
+++ screens/notes/NotesScreen.js
@@ -1,18 +1,18 @@
 import getStyle from "../../components/StyleSheet"
 import {View, Text} from "react-native";
 import {useSettings} from "../../contexts/SettingsContext.js";
-import { useTranslation } from 'react-i18next';
-import {useEffect} from "react";
-import {useNavigation} from "@react-navigation/native";
+import {useTranslation} from 'react-i18next';
+import {useCallback, useEffect, useState} from "react";
+import {useFocusEffect, useNavigation} from "@react-navigation/native";
 import i18next from "i18next";
+import DropDownPicker from "react-native-dropdown-picker";
+import {useLocations} from "../../contexts/LocationsContext";
 
-export default function NotesScreen({ route }) {
+export default function NotesScreen({route}) {
 
     const {t} = useTranslation();
 
     const navigator = useNavigation();
-
-    const {locationId} = route.params;
 
     const {settings} = useSettings();
 
@@ -20,14 +20,63 @@ export default function NotesScreen({ route }) {
 
     const styles = getStyle(theme);
 
+    const [locationId, setLocationId] = useState(route.params?.locationId ?? 0);
+
+    const [openLocation, setOpenLocation] = useState(false);
+    const [locationValue, setLocationValue] = useState(locationId);
+    const [locationItems, setLocationItems] = useState([]);
+
+    const {locations} = useLocations();
+
     useEffect(() => {
         i18next.changeLanguage(settings.language);
         navigator.getParent().setOptions({title: t('NOTES.TITLE')});
     }, [settings]);
 
+    useEffect(() => {
+
+        if (locations) {
+
+            let parsedLocations = [];
+
+            for (const location of locations) {
+
+                parsedLocations.push({label: location.name, value: location.id})
+
+            }
+
+            setLocationItems(parsedLocations);
+
+        }
+
+    }, [locations]);
+
+    useFocusEffect(
+        useCallback(() => {
+
+            if (route.params) {
+                setLocationId(route.params.locationId);
+                setLocationValue(route.params.locationId);
+            }
+
+        }, [route.params])
+    );
+
+
     return (
         <View style={styles.container}>
+
             <Text style={styles.text}>{t('NOTES.TITLE')}</Text>
+
+            <DropDownPicker
+                setValue={setLocationValue}
+                value={locationValue}
+                items={locationItems}
+                setItems={setLocationItems}
+                open={openLocation}
+                setOpen={setOpenLocation}
+            />
+
         </View>
     );
 
