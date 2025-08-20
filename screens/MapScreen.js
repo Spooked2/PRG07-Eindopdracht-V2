30e64286bc2e4c6df9464d68eccc15e40a93bff0
sander <sanderlandm@gmail.com>
2025-08-20 18:15:09 +0200

    ADD app now asks permission for the user's location and shows it on the map if given

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,7 +3,7 @@ import {View} from "react-native";
 import {useSettings} from "../contexts/SettingsContext.js";
 import {useTranslation} from 'react-i18next';
 import {useNavigation} from "@react-navigation/native";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import i18next from "i18next";
 import MapView, {Marker} from "react-native-maps";
 import {useLocations} from "../contexts/LocationsContext";
@@ -23,10 +23,28 @@ export default function MapScreen() {
 
     const {locations} = useLocations();
 
+    const [allowUserLocation, setAllowUserLocation] = useState(false);
+
     useEffect(() => {
         i18next.changeLanguage(settings.language);
         navigator.setOptions({title: t('MAP.TITLE')});
     }, [settings]);
+
+    useEffect(() => {
+
+        const handlePermissions = async () => {
+
+            const {status} = await Location.requestForegroundPermissionsAsync();
+
+            if (status === 'granted') {
+                setAllowUserLocation(true);
+            }
+
+        }
+
+        handlePermissions();
+
+    }, []);
 
     return locations ? (
         <View style={styles.container}>
@@ -40,17 +58,17 @@ export default function MapScreen() {
                     longitudeDelta: 0.002
                 }}
                 mapType={"standard"}
-                showsUserLocation={true}
+                showsUserLocation={allowUserLocation}
             >
 
                 {locations.map(location => (
-                        <Marker
-                            key={location.id}
-                            coordinate={location.coordinates}
-                            title={location.name}
-                            description={''}
-                        />
-                    ))}
+                    <Marker
+                        key={location.id}
+                        coordinate={location.coordinates}
+                        title={location.name}
+                        description={''}
+                    />
+                ))}
 
             </MapView>
 
